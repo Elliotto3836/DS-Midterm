8388d08c67f75f027e4b312f2fe7f8169c18f6e7
Elliot Zheng <56090975+Elliotto3836@users.noreply.github.com>
2025-03-03 16:14:08 +0000

    Cleaning up red slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ import streamlit.components.v1 as components
 #For this project we will use
 #https://www.kaggle.com/datasets/uom190346a/sleep-health-and-lifestyle-dataset
 
-
 #to push
 #git add .
 #git commit -am "Message here"
@@ -20,13 +19,46 @@ import streamlit.components.v1 as components
 #git pull to pull
 
 #st.title("Welcome!")
+
+
 st.markdown("# :rainbow[How to Improve Sleep? An analysis of Sleep and Health Data.]")
 
 st.header("Elliot Zheng and Katie Liao",divider="blue")
 
+st.image("Sleeping.jpg")
+
 df = pd.read_csv("health.csv")
 
+st.header("",divider="blue")
+
 app_mode = st.sidebar.selectbox("Select a page",["Introduction","Data Visualization","Predictions"])
+
+st.markdown("""<style>
+div[data-baseweb="tab-list"] button {
+    color: #60b5ff !important;  /* Nord blue text color */
+}
+
+div[data-baseweb="tab-list"] button[aria-selected="true"] {
+    color: #60b5ff !important; 
+    background-color: transparent !important; 
+    font-weight: bold !important;
+}
+
+.st-gb {
+    background-color: #60b5ff !important;  /* Change the text color to blue */
+}
+            
+.st-gb {
+    background: #60b5ff !important;  /* Change the text color to blue */
+}
+
+.st-ej {
+    background-color: #60b5ff !important;  /* Change the text color to blue */
+}
+
+.st-gb ul li {
+    background-color: #60b5ff !important;  /* Change bullet points color to blue */
+} </style>""", unsafe_allow_html=True)
 
 if app_mode == "Introduction":
     rain(emoji="💤",font_size=54,falling_speed=5,animation_length="10",)
@@ -60,29 +92,7 @@ if app_mode == "Introduction":
 
 if app_mode == "Data Visualization":
     st.markdown("# Data Visualization:")
-    st.markdown("""
-    <style>
-        div[data-baseweb="tab-list"] button {
-            color: #60b5ff !important;  /* Nord blue text color */
-        }
-        
-        div[data-baseweb="tab-list"] button[aria-selected="true"] {
-            color: #60b5ff !important; 
-            background-color: transparent !important; 
-            font-weight: bold !important;
-        }
 
-        .st-gb {
-            background-color: #60b5ff !important;  /* Change the text color to blue */
-        }
-
-        .st-ej {
-            background-color: #60b5ff !important;  /* Change the text color to blue */
-        }
-
-        .st-gb ul li {
-            background-color: #60b5ff !important;  /* Change bullet points color to blue */
-        } </style>""", unsafe_allow_html=True)
 
     pairPlot,countPlots, BoxAndWhisker, PieChart= st.tabs(["Pairplot","Count Plots", "Box and Whisker Plots", "Pie Charts"])
 
